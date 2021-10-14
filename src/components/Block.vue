cbab26e63323c40d2d46a50e82a70da2d7a5ca70
Masimthembe Ndabeni <mndabeni6@gmail.com>
2021-10-14 13:33:26 +0200

    first commit

---
+++ src/components/Block.vue
@@ -0,0 +1,52 @@
+<template>
+  <div class="block" v-if="showBlock" @click="stopTimer">
+      click me
+      </div>
+</template>
+
+<script>
+export default {
+    props: ['delay'],
+    data() {
+        return {
+            showBlock: false,
+            timer: null,
+            reationTime: 0
+        }
+    },
+    mounted() {
+        console.log ('components mounted')
+        setTimeout(() => {
+            this.showBlock =  true
+            this.starTimer()
+        }, this.delay)
+
+    },
+    
+    methods: {
+        starTimer() {
+            this.timer = setInterval(() => {
+             this.reactionTime += 10
+            }, 10)
+        },
+        stopTimer() {
+            clearInterval(this.timer)
+            this.$emit('end', this.reactionTime)
+
+        }
+    }
+
+}
+</script>
+
+<style>
+.block {
+    width: 400px;
+    border-radius: 20px;
+    background-color: #0faf87;
+    text-align: center;
+    padding: 100px 0;
+    margin: 40px auto;
+}
+
+</style>
